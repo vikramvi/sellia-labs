f69a3cbc5116e762f0e531547dfeb0c28b023225
Bhavik S <bnshah.dev@gmail.com>
2020-11-30 16:14:11 +0530

    gitignore updated

--- packages/web/.next/static/chunks/webpack.js
+++ packages/web/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "602e5396a006a7f7e27b";
+/******/ 	var hotCurrentHash = "06a8a6def982e5ccfb8d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
