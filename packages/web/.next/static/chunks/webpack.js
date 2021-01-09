5bd329e0dfd4e377ea139156572ef613625d110c
Bhavik S <bnshah.dev@gmail.com>
2021-01-09 18:11:55 +0530

    author display

--- packages/web/.next/static/chunks/webpack.js
+++ packages/web/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7c9c04bef1cfaabd2c19";
+/******/ 	var hotCurrentHash = "6397c1c90f373b960801";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
