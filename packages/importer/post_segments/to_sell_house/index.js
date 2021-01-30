3204d13c10dfdf3a305a6a8b1e8cb8a693283e4c
Bhavik S <bnshah.dev@gmail.com>
2021-01-30 17:09:35 +0530

    sell house importer updated

--- packages/importer/post_segments/to_sell_house/index.js
+++ packages/importer/post_segments/to_sell_house/index.js
@@ -1,7 +1,7 @@
-const admin = require('firebase-admin');
-const fs = require('fs');
-const serviceAccount = require('../../headless-graphql-firebase-config.json');
-const dotenv = require('dotenv');
+const admin = require("firebase-admin");
+const fs = require("fs");
+const serviceAccount = require("../../headless-graphql-firebase-config.json");
+const dotenv = require("dotenv");
 dotenv.config();
 
 // You should replace databaseURL with your own
@@ -15,48 +15,56 @@ db.settings({ timestampsInSnapshots: true });
 
 const configData = [
   {
-    title: 'Title',
-    type: 'textField',
+    title: "Title",
+    type: "textField",
+    key: "title",
   },
   {
     list: [
       {
-        title: 'Beds',
-        type: 'textBox',
+        title: "Beds",
+        type: "textBox",
+        key: "beds",
       },
       {
-        title: 'Baths',
-        type: 'textBox',
+        title: "Baths",
+        type: "textBox",
+        key: "baths",
       },
       {
-        title: 'Lot Size',
-        type: 'textBox',
+        title: "Lot Size",
+        type: "textBox",
+        key: "lotSize",
       },
       {
-        title: 'Sq. Ft.',
-        type: 'textBox',
+        title: "Sq. Ft.",
+        type: "textBox",
+        key: "sqft",
       },
     ],
-    title: '',
-    type: 'rowContainer',
+    title: "",
+    type: "rowContainer",
   },
   {
     list: [
       {
-        title: 'Price',
-        type: 'currency',
+        title: "Price",
+        type: "currency",
+        key: "currency",
       },
     ],
-    title: 'Price',
-    type: 'rowContainer',
+    title: "Price",
+    type: "rowContainer",
   },
   {
-    type: 'textBox',
-    title: 'Zip Code',
+    type: "textBox",
+    title: "Zip Code",
+    key: "zip",
   },
   {
-    type: 'textDescription',
-    title: 'Description',
+    type: "textDescription",
+    title: "Description",
+    key: "content",
   },
 ];
 
@@ -64,8 +72,8 @@ function updateCategoryList_toSell() {
   var usersUpdate = {};
   usersUpdate[`sections`] = configData;
 
-  db.collection('post_segments')
-    .doc('to_sell_house')
+  db.collection("post_segments")
+    .doc("to_sell_house")
     .update(usersUpdate);
 }
 
