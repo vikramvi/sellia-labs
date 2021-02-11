a6cfb64d8d7b292393de8d6e115bb81efbfc8acc
Bhavik S <bnshah.dev@gmail.com>
2021-02-11 13:31:54 +0530

    Post a message, event, poll  added

--- packages/web/containers/Home/Feed/index.js
+++ packages/web/containers/Home/Feed/index.js
@@ -17,6 +17,10 @@ import OnError from "../../../components/OnError";
 import { FeedContext } from "../../../contexts/FeedContext";
 import { useContext, useEffect } from "react";
 import { GET_CATEGORY_POST } from "core/graphql/CategoryPost.query";
+import "./style.css";
+import Img from "react-image";
+import profileImg from "core/static/images/user-placeholder.svg";
+import { openModal, closeModal } from "@redq/reuse-modal";
 
 export default function Feed({ userId, isLoggedIn }) {
   const { state, dispatch } = useContext(FeedContext);
@@ -80,40 +84,63 @@ export default function Feed({ userId, isLoggedIn }) {
     const { seconds } = item.createdAt || {};
     const { url, largeUrl } = item.image || {};
 
+    const handleAddPost = () => {
+      openModal({
+        overlayClassName: "customeOverlayClass",
+        closeOnClickOutside: false,
+        componentProps: {
+          data: {
+            closeModal,
+            userId,
+          },
+        },
+        config: {
+          className: "addPostNewModal",
+          height: "auto",
+          width: "50%",
+          transition: {
+            tension: 150,
+          },
+        },
+        component: AddPostModal,
+      });
+    };
+
     return (
       // <Link
       //   href={`${SINGLE_POST_PAGE}/[slug]`}
       //   as={`${SINGLE_POST_PAGE}/${slug}`}
       // >
-      <a>
-        <FeedPostCard
-          style={{
-            flexDirection: "row",
-            display: "flex",
-            justifyContent: "flex-start",
-          }}
-          imageStyle={{
-            marginRight: 20,
-          }}
-          currency={CURRENCY}
-          title={title}
-          price={price}
-          imageSrc={[url, largeUrl]}
-          author={name}
-          userId={userId}
-          createdAt={seconds}
-          content={content}
-          item={item}
-          avatar={image?.url}
-          condition={condition}
-          originalPrice={originalPrice}
-          isLoggedIn={isLoggedIn}
-          authorId={authorId}
-          postStatus={status}
-          id={id}
-        />
-      </a>
-      // </Link>
+      <>
+        <a>
+          <FeedPostCard
+            style={{
+              flexDirection: "row",
+              display: "flex",
+              justifyContent: "flex-start",
+            }}
+            imageStyle={{
+              marginRight: 20,
+            }}
+            currency={CURRENCY}
+            title={title}
+            price={price}
+            imageSrc={[url, largeUrl]}
+            author={name}
+            userId={userId}
+            createdAt={seconds}
+            content={content}
+            item={item}
+            avatar={image?.url}
+            condition={condition}
+            originalPrice={originalPrice}
+            isLoggedIn={isLoggedIn}
+            authorId={authorId}
+            postStatus={status}
+            id={id}
+          />
+        </a>
+      </>
     );
   };
 
@@ -121,6 +148,35 @@ export default function Feed({ userId, isLoggedIn }) {
     <>
       <NavSidebar></NavSidebar>
       <Box mt={20} ml={"25%"} mr={"25%"}>
+        <div class="css-1dkvlfs">
+          <Box
+            mb={15}
+            aria-live="off"
+            id="main_content"
+            class="_19x6ASDS _1eW-tOzA"
+          >
+            <span class="_1EhANPqp">
+              <div class="avatar">
+                <Img
+                  src={profileImg}
+                  style={{ width: 35, height: 35, borderRadius: "50%" }}
+                  loader={<img src={profileImg} alt="profile picture" />}
+                  unloader={<img src={profileImg} alt="profile picture" />}
+                />
+              </div>
+            </span>
+            <div class="_3zGsrPl_ _2wCEotpQ">
+              <div class="_1IukxMPo _2YwTbt9g _1uAKrEJw">
+                <span>
+                  Post a <span class="prompt-bold">message</span>,{" "}
+                  <span class="prompt-bold">event</span>,{" "}
+                  <span class="prompt-bold">poll</span> or{" "}
+                  <span class="prompt-bold">alert</span> to your neighborhood
+                </span>
+              </div>
+            </div>
+          </Box>
+        </div>
         {!recentPosts ? (
           <NoItemFound />
         ) : (
